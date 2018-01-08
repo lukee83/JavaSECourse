9db80b0a10d04e982f1b9ef89d7b48fc72406f3a
Istvan <lukacs.istvan.83@gmail.com>
2018-01-08 15:50:14 +0100

    a

--- FirstJavaProject/src/hu/masterfield/bases/HelloWorld.java
+++ FirstJavaProject/src/hu/masterfield/bases/HelloWorld.java
@@ -1,10 +1,18 @@
 package hu.masterfield.bases;
 
-public class HelloWorld {
+import java.lang.instrument.Instrumentation;
 
+public class HelloWorld {
+/*
 	public static void main(String[] args) {
 	      // Prints "Hello, World" in the terminal window.
-	      System.out.println("Hello, World");
+	      // System.out.println("Hello, World");
+	      System.out.println("syso shortcut");
+	    
+	      Object o = 5;
+	      
+	      System.out.println(o);
+	      System.out.println(o.getClass().getName());
 	   }
-	
+	*/
 }
